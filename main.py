2bc22f9fe55f165e5c7154b8bc0404b0d9901544
Klinovitsky <klinovitsky@gmail.com>
2023-12-23 19:18:34 +0300

    Ver.2 + Login input and single password generation + Encrypt function + Add module for decrypt + Cipher hack function.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 # Functions:
 #     get_config(file) - > object
 #     settings(object, file) -> list
+#     encrypt(string, int) -> string
 #     generate(object) -> object
 # Misc variables:
 #     master_password
@@ -21,6 +22,7 @@ import json
 import getpass
 import datetime
 import zipfile
+import crypto
 
 
 def get_config():
@@ -48,25 +50,69 @@ def add():
     """Calculate the sum."""
 
 
-def encrypt(master_password, second_password, data):
-    """Encrypts data with second and master password"""
+# def encrypt(master_password, second_password, data):
+#     """Encrypts data with second and master password"""
+def encrypt(text, shift):
+    """Software implementation of the Caesar encryption algorithm"""
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+
+    return result
 
 
-def decrypt(master_password, second_password, data):
-    """Decrypts data with second and master password"""
+def hack_cipher(text):
+    """Find the key in a strange way (Hack algorithm?)"""
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    assumptions = {}
+    # Attempt to hack the cipher
+    for key in range(len(letters)):
+        translated = ''
+        for symbol in text:
+            if symbol in letters:
+                num = letters.find(symbol)
+                num = num - key
+                if num < 0:
+                    num = num + len(letters)
+                    translated = translated + letters[num]
+                else:
+                    translated = translated + symbol
+        assumptions[key] = translated
+        print('Decrypt key #%s: %s' % (key, translated))
+
+    return assumptions[30]  # Take the key with the right answer (always key # 30)
 
 
 def generate():
     """Generate passwords and return a list of passwords and their number"""
-    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    number = int(input('Enter the number of passwords: '))
 
+    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    single_password = ''
+    login = 'klinovitsky@gmail.com'     # Or this way: login = input('login ')
+    passwords_list = []
+
+    length = int(input('Enter password length: ' + "\n"))
+
+    # Way 1. Single password
+    for _ in range(length):
+        single_password += random.choice(list(chars))  # Chars for random
+    print(f'Your single password for login "{login}", '
+          f'with length "{length}" is: {single_password}')
+
+    # Way 2. Multiple passwords
+    print('-------------------------------------------')
+    number = int(input('You can generate other passwords of the same length.\n'
+                       'Enter the number of passwords: ' + "\n"))
     if number == 0:     # I did not return to re-enter the value because this condition for fun
         print('You have entered 0 passwords. Exit the program')
         raise SystemExit
-
-    length = int(input('Enter password length: '))
-    passwords_list = []
 
     for _ in range(number):     # To avoid unused variable warning in Pylint, replaced by (‘_’).
         password = ''
@@ -85,9 +131,7 @@ def generate():
 def output(passwords_list, number, config):
     """Final information about the program"""
     print(f'Configuration: {config}')
-    print(f'The number of passwords generated: {number}')
-    print(f'Password(s): {passwords_list}')
-
+    print(f'Your {number} password(s): {passwords_list}')
 
     # Workpiece 1.
     # colors = ['red', 'green', 'blue', 'yellow']
@@ -116,6 +160,33 @@ def main():
     add()
     passwords_list, number = generate()
     output(passwords_list, number, config)
+
+    # Encrypt text test
+    text = str(input('Enter the text for pass (no spaces please): ' + "\n"))   # Spaces and decrypt
+    # text = 'ABCD'   # After encrypt should be 'EFGH'
+    shift = 4
+    result_encrypt = encrypt(text, shift)   # Function returns 1 value - text
+    print("Plain Text : " + text)
+    print("Shift pattern : " + str(shift))
+    print("Cipher result: ", result_encrypt)
+    print('-------------------------------------------')
+
+    # result_test = encrypt('CEASER CIPHER DEMONSTRATION', 4)
+    # print('Cipher for string "CEASER CIPHER DEMONSTRATION" with shift "4" is:', result_test)
+
+    # Decrypt encrypted text
+    # encrypted_text = 'EFGH'
+    encrypted_text = result_encrypt
+    result_decrypt = hack_cipher(encrypted_text)
+    print('-------------------------------------------')
+    print('Hack complete. The key number #30 is:', result_decrypt)
+    # result_decrypt = hack_cipher(encrypted_text)
+    # print('result_decrypt, last value', result_decrypt)
+
+    #Module two
+    print('-------------------------------------------')
+    decrypt_flag = 2
+    crypto.encrypt_decrypt(encrypted_text, decrypt_flag, shift)
 
 
 # Press the green button in the gutter to run the script.
