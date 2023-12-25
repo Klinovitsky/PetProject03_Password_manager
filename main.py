b42d671d0aa7dcb39730e90210431e0ba90b419c
Klinovitsky <klinovitsky@gmail.com>
2023-12-25 18:40:41 +0300

    Ver 2.3. added user input function, refactoring, test and TODO

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ def get_config():
     with open('settings.json', encoding='utf-8') as fh:  # Open the file for reading
         data = json.load(fh)  # Load data from the file into the data dictionary
     data['Path'] = '"' + directory + '"'
-    print("Data from JSON file:\n", data)
-    print('-------------------------------------------')
+    print("... Data from JSON file:", data)
 
     # master_password = ''
 
@@ -51,12 +50,49 @@ def get_config():
     #     # 'setpassword' method is used to give a password to the 'Zip'
     #     file.setpassword(pwd=bytes(second_password, 'utf-8'))
     #     file.extractall()
+    print('... ------------------------------------------- End of get_config()')
 
     return data
 
 
-def add():
-    """Calculate the sum."""
+def user_input():
+    """Take data from the user and save them."""
+    def print_choice():     # Testing a nested function
+        print('Your choice is:', login_choice, 'Login:', login)
+
+    login = ''
+    login_choice = int(input('\nType your choose. My logins: 1. Mail.ru 2. Gmail.com 3. Цифра, '
+                             '4. Cell phone, 5. Input manually \n'))
+    if login_choice == 1:
+        login = 'vas_ky@mail.ru'
+        print_choice()
+    elif login_choice == 2:
+        login = 'klino_vitsky@gmail.com'
+        print_choice()
+    elif login_choice == 3:
+        login = 'vladi_mir.klinovickiy@zyfra.com'
+        print_choice()
+    elif login_choice == 4:
+        login = '123321_89059077777'
+        print_choice()
+    elif login_choice == 5:
+        login = input('Input login: \n')
+        print_choice()
+    else:
+        print('There is no such choice. Try again.')
+        main()
+    # TODO. Save user_login to dict
+
+    resource = str(input('Type or paste the name of the resource for which '
+                         'a password is required: \n'))
+    # TODO. Get and save recourse, For example "google.com"
+
+    password_length = int(input('Enter password length: \n'))
+
+    # TODO. Temporary return variables from function (see return)
+    print('... ------------------------------------------- End of user_input()')
+
+    return login, resource, password_length
 
 
 # def encrypt(master_password, second_password, data):
@@ -99,28 +135,26 @@ def hack_cipher(text):
     return assumptions[30]  # Take the key with the right answer (always key # 30)
 
 
-def generate():
+def generate(resource, login, password_length):
     """Generate passwords and return a list of passwords and their number"""
 
-    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    length = password_length
     single_password = ''
-    login = 'klinovitsky@gmail.com'     # Or this way: login = input('login ')
     passwords_list = []
-
-    length = int(input('Enter password length: ' + "\n"))
+    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
     # Way 1. Single password
     for _ in range(length):
         single_password += random.choice(list(chars))  # Chars for random
-    print(f'Your single password for login "{login}", '
+    print(f'Your single password for resource: "{resource}" , login: "{login}", '
           f'with length "{length}" is: {single_password}')
 
     # Way 2. Multiple passwords
-    print('-------------------------------------------')
-    number = int(input('You can generate other passwords of the same length.\n'
-                       'Enter the number of passwords: ' + "\n"))
+    print('... ------------------------------------------- generate(), Start Way 2')
+    number = int(input('Generate other passwords of the same length?\n'
+                       'Please, enter the number of passwords (or type "0" to exit): ' + "\n"))
     if number == 0:     # I did not return to re-enter the value because this condition for fun
-        print('You have entered 0 passwords. Exit the program')
+        print('You have entered "0" passwords. Exit the program')
         raise SystemExit
 
     for _ in range(number):     # To avoid unused variable warning in Pylint, replaced by (‘_’).
@@ -139,7 +173,7 @@ def generate():
 
 def output(passwords_list, number, options):
     """Final information about the program"""
-    print(f'Configuration: {options}')
+    print(f'... Configuration from JSON in output(): {options}')
     print(f'Your {number} password(s): {passwords_list}')
 
     # Workpiece 1.
@@ -165,20 +199,21 @@ def output(passwords_list, number, options):
 
 def main():
     """I can quickly comment out the function call from here"""
+    login, resource, password_length = user_input()
     options = get_config()
-    add()
-    passwords_list, number = generate()
+    passwords_list, number = generate(resource, login, password_length)
     output(passwords_list, number, options)
 
+    print('... ------------------------------------------- main()')
     # Encrypt text test
-    text = str(input('Enter the text for pass (no spaces please): ' + "\n"))   # Spaces and decrypt
+    # TODO. Spaces and decrypt
+    text = str(input('Enter the text to encrypt the password (no spaces please): ' + "\n"))
     # text = 'ABCD'   # After encrypt should be 'EFGH'
     shift = 4
     result_encrypt = encrypt(text, shift)   # Function returns 1 value - text
     print("Plain Text : " + text)
     print("Shift pattern : " + str(shift))
     print("Cipher result: ", result_encrypt)
-    print('-------------------------------------------')
 
     # result_test = encrypt('CEASER CIPHER DEMONSTRATION', 4)
     # print('Cipher for string "CEASER CIPHER DEMONSTRATION" with shift "4" is:', result_test)
@@ -187,16 +222,16 @@ def main():
     # encrypted_text = 'EFGH'
     encrypted_text = result_encrypt
     result_decrypt = hack_cipher(encrypted_text)
-    print('-------------------------------------------')
     print('Hack complete. The key number #30 is:', result_decrypt)
+    # TODO. Test result: DromTestFindGen - > DTFG after Hack
     # result_decrypt = hack_cipher(encrypted_text)
     # print('result_decrypt, last value', result_decrypt)
 
     #Module two
-    print('-------------------------------------------')
+    print('... ------------------------------------------- main() -> crypto.py')
     decrypt_flag = 2
     crypto.encrypt_decrypt(encrypted_text, decrypt_flag, shift)
-
+    # TODO. Test result: DromTestFindGen -> DROMTESTFINDGEN (all capitals after decrypt!)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
